00f02c356bfd2ec85614442cd560ecaac647cda5
ndragun92 <nemanjadragun92@gmail.com>
2023-03-13 21:39:36 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Nuxt 2 Script Setup Example</h1>
+    <button type="button" @click="counter++">Increase</button>
+    <div>
+      {{counter}}
+    </div>
+  </div>
+</template>
+
+<!--<script lang="ts">-->
+<!--import Vue from 'vue'-->
+
+<!--export default Vue.extend({-->
+<!--  name: 'IndexPage',-->
+<!--})-->
+<!--</script>-->
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const counter = ref(0)
+</script>
